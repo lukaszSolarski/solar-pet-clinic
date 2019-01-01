5fa1bb0e42fd5fd40fcdd90190fb28d9a37c29fe
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-01 15:49:39 +0100

    add PetType Service Closes #29

---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.solar.springframework5.solarpetclinic.services;
+
+import com.solar.springframework5.solarpetclinic.data.PetType;
+
+public interface PetTypeService extends BasicCrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.solar.springframework5.solarpetclinic.services.map;
+
+import com.solar.springframework5.solarpetclinic.data.PetType;
+import com.solar.springframework5.solarpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
