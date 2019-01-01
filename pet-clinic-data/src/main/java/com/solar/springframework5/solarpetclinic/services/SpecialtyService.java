2d122c3243dd36179efe0044112e8c77834a690a
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-01 17:02:27 +0100

    add Specialty Service and it's implementation Closes #31

---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.solar.springframework5.solarpetclinic.services;
+
+import com.solar.springframework5.solarpetclinic.data.Specialty;
+
+public interface SpecialtyService extends BasicCrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package com.solar.springframework5.solarpetclinic.services.map;
+
+import com.solar.springframework5.solarpetclinic.data.Specialty;
+import com.solar.springframework5.solarpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
