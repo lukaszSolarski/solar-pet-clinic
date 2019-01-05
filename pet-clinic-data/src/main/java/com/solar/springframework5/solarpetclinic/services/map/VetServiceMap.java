8016d695e0fed5fddc4aa0885a20e55d701a9997
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-05 11:54:05 +0100

    add specialties to vet on startup Closes #34

--- pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.solar.springframework5.solarpetclinic.services.map;
 
+import com.solar.springframework5.solarpetclinic.data.Specialty;
 import com.solar.springframework5.solarpetclinic.data.Vet;
+import com.solar.springframework5.solarpetclinic.services.SpecialtyService;
 import com.solar.springframework5.solarpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Vet findById(Long id) {
         return super.findById(id);
@@ -20,7 +29,19 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
-        return super.save(vet);
+        if (vet != null) {
+            if ((vet.getSpecialties() != null) & (vet.getSpecialties().size() > 0)) {
+                vet.getSpecialties().forEach(specialty -> {
+                    if (specialty.getId() == null) {
+                        Specialty savedSpecialty = specialtyService.save(specialty);
+                        specialty.setId(savedSpecialty.getId());
+                    }
+                });
+            }
+            return super.save(vet);
+        } else {
+            throw new RuntimeException("Vet cannot be null!");
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/solar/springframework5/solarpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/solar/springframework5/solarpetclinic/bootstrap/DataInitializer.java
@@ -1,11 +1,9 @@
 package com.solar.springframework5.solarpetclinic.bootstrap;
 
-import com.solar.springframework5.solarpetclinic.data.Owner;
-import com.solar.springframework5.solarpetclinic.data.Pet;
-import com.solar.springframework5.solarpetclinic.data.PetType;
-import com.solar.springframework5.solarpetclinic.data.Vet;
+import com.solar.springframework5.solarpetclinic.data.*;
 import com.solar.springframework5.solarpetclinic.services.OwnerService;
 import com.solar.springframework5.solarpetclinic.services.PetTypeService;
+import com.solar.springframework5.solarpetclinic.services.SpecialtyService;
 import com.solar.springframework5.solarpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -21,19 +19,32 @@ public class DataInitializer implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialtyService specialtyService;
 
     /*
     @Autowired is not required. Since Spring 4.3 (?) Dependency injection by constructor will automatically
     act like the '@Autowired' annotation was provided.
      */
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialtyService = specialtyService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        /*
+        We have to save data only if database is empty. When we'll be using real MySQL database,
+        we shouldn't save this to db because it will cause multiplying data in database.
+         */
+        int countPetTypes = petTypeService.findAll().size();
+        if (countPetTypes == 0) {
+            loadDefaultData();
+        }
+    }
+
+    private void loadDefaultData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -41,8 +52,19 @@ public class DataInitializer implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
-
         System.out.println("Pet types loaded...");
+
+        Specialty radiology = new Specialty();
+        radiology.setDescription("Radiology");
+        Specialty surgery = new Specialty();
+        surgery.setDescription("surgery");
+        Specialty dentistry = new Specialty();
+        dentistry.setDescription("dentistry");
+        Specialty savedRadiology = specialtyService.save(radiology);
+        Specialty savedSurgary = specialtyService.save(surgery);
+        Specialty savedDentistry = specialtyService.save(dentistry);
+        System.out.println("Specialties saved");
+
         Owner o1 = new Owner();
         o1.setFirstName("Luke");
         o1.setLastName("Skywalker");
@@ -76,10 +98,13 @@ public class DataInitializer implements CommandLineRunner {
         Vet v1 = new Vet();
         v1.setFirstName("Chuck");
         v1.setLastName("Norris");
+        v1.getSpecialties().add(savedRadiology);
 
         Vet v2 = new Vet();
         v2.setFirstName("Donald");
         v2.setLastName("Duck");
+        v2.getSpecialties().add(savedSurgary);
+        v2.getSpecialties().add(savedDentistry);
 
         vetService.save(v1);
         vetService.save(v2);
