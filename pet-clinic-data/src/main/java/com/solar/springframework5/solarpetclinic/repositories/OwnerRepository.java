ace6fcc73ada3203b709d43aebfbc19395aee674
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-12 18:13:51 +0100

    Create Spring data JPA Service for Owners Closes #40

--- pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.solar.springframework5.solarpetclinic.data.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
