a3309dc57bba106834a4b883a4f8e046ae08a41d
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-14 23:51:52 +0100

    Create a Visit Map Service Closes #46

---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.solar.springframework5.solarpetclinic.services;
+
+import com.solar.springframework5.solarpetclinic.data.Visit;
+
+public interface VisitService extends BasicCrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,38 @@
+package com.solar.springframework5.solarpetclinic.services.map;
+
+import com.solar.springframework5.solarpetclinic.data.Visit;
+import com.solar.springframework5.solarpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Problem with saving visit " + visit.toString());
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
