88fc09f6c8a7811ea681ee023813dd7816192817
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-14 23:57:46 +0100

    Create Spring Data JPA service for Visit Closes #47

---
+++ pet-clinic-data/src/main/java/com/solar/springframework5/solarpetclinic/services/springdatajpa/VisitServiceJpa.java
@@ -0,0 +1,47 @@
+package com.solar.springframework5.solarpetclinic.services.springdatajpa;
+
+import com.solar.springframework5.solarpetclinic.data.Visit;
+import com.solar.springframework5.solarpetclinic.repositories.VisitRepository;
+import com.solar.springframework5.solarpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitServiceJpa implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJpa(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
