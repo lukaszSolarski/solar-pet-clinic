d8e6b59c527c1e14cbcd0a07fc1331bd8535e08f
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-15 11:22:21 +0100

    Load Visit data on startup Closes #48

--- pet-clinic-web/src/main/java/com/solar/springframework5/solarpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/solar/springframework5/solarpetclinic/bootstrap/DataInitializer.java
@@ -1,10 +1,7 @@
 package com.solar.springframework5.solarpetclinic.bootstrap;
 
 import com.solar.springframework5.solarpetclinic.data.*;
-import com.solar.springframework5.solarpetclinic.services.OwnerService;
-import com.solar.springframework5.solarpetclinic.services.PetTypeService;
-import com.solar.springframework5.solarpetclinic.services.SpecialtyService;
-import com.solar.springframework5.solarpetclinic.services.VetService;
+import com.solar.springframework5.solarpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +17,19 @@ public class DataInitializer implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     /*
     @Autowired is not required. Since Spring 4.3 (?) Dependency injection by constructor will automatically
     act like the '@Autowired' annotation was provided.
      */
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                           SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -109,5 +109,18 @@ public class DataInitializer implements CommandLineRunner {
         vetService.save(v1);
         vetService.save(v2);
         System.out.println("Vets loaded...");
+
+        Visit visit1 = new Visit();
+        visit1.setDate(LocalDate.now());
+        visit1.setDescription("Description of first visit of the Cat");
+        visit1.setPet(johnsCat);
+
+        Visit visit2 = new Visit();
+        visit2.setDate(LocalDate.of(2018, 3, 24));
+        visit2.setDescription("This is first visit of a Luke's dog in the app");
+        visit2.setPet(lukesDog);
+
+        visitService.save(visit1);
+        visitService.save(visit2);
     }
 }
