4162059d2f2141583e84f045b4d2947b419e4607
Lukasz.Solarski <lukasz.solar@gmail.com>
2019-01-19 20:22:29 +0100

    Write Spring MVC tests for Owner Controller add Mock dependency to pet-clinic-web Closes #52

---
+++ pet-clinic-web/src/test/java/com/solar/springframework5/solarpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,76 @@
+package com.solar.springframework5.solarpetclinic.controllers;
+
+import com.solar.springframework5.solarpetclinic.data.Owner;
+import com.solar.springframework5.solarpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    OwnerController ownerController;
+    Set<Owner> owners;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        ownerController = new OwnerController(ownerService);
+        owners = new HashSet<>();
+        Owner o1 = new Owner();
+        o1.setId(1L);
+        Owner o2 = new Owner();
+        o2.setId(2L);
+        owners.add(o1);
+        owners.add(o2);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index.html"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void listOwnersIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index.html"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void findOwner() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notimplemented"));
+        verifyZeroInteractions(ownerService);
+    }
+}
